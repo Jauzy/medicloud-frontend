d44dc4888e2099aaed6c898f299c94a42a9d9132
michaeliwe17 <148730987+michaeliwe17@users.noreply.github.com>
2023-12-30 21:17:01 +0700

    Update baseURL.js

--- src/state/baseURL.js
+++ src/state/baseURL.js
@@ -1,3 +1,3 @@
 import axios from "axios";
-export default axios.create({baseURL: 'https://medicloud-id.herokuapp.com/'})
+export default axios.create({baseURL: 'http://103.183.75.190/'})
 // export default axios.create({baseURL: 'http://localhost:8001/'})
